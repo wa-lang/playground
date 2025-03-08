a03b8b6bd82a8c2f93ce8bfeb65a2ec7eb5b5809
xxx <humengmingxx@gmail.com>
2025-03-08 21:07:17 +0800

    chore: up title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <header className="h-14 border-b flex items-center px-6 flex-shrink-0">
       <Logo className="w-6 h-6 mr-2" />
-      <h1 className="text-lg font-semibold mr-auto">Wa Playground</h1>
+      <h1 className="text-lg font-semibold mr-auto">WA Playground</h1>
       <HomeIcon
         className="size-[1.2rem] mr-3 cursor-pointer"
         onClick={() => window.open('https://wa-lang.org', '_blank')}
