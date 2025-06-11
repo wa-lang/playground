ea08e10410b12921a7709730c2ccb871e7a0c50d
xxx <humengmingxx@gmail.com>
2025-06-11 21:58:42 +0800

    feat: optimize monaco load

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
+import { loader } from '@monaco-editor/react'
+import * as monaco from 'monaco-editor'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './tailwind.css'
+
+loader.config({ monaco })
 
 createRoot(document.getElementById('root')!).render(<App />)
